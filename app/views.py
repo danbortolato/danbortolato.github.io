3206e0be16152f6f745dff1695746718e4d1fdc1
Daniel Bortolato <daniel.bortolato@gmail.com>
2022-07-16 22:11:44 -0300

    views update

--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
 from multiprocessing import context
 
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+    sucess_url = reverse_lazy('index')
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
